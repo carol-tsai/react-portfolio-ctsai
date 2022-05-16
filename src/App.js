6dd22535c45b72c621815a0efa038518095a3a60
Carol <carol.tsai0224@gmail.com>
2022-05-16 11:35:51 -0400

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 function App() {
-  return <PortfolioContainer />;
+  return (
+    <div>
+      <Header/>
+      <PortfolioContainer/>
+      <Footer/>
+    </div>
+    
+
+    );
 }
 
 export default App;
