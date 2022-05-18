0c626ac98f1bdd7e9aa317f5194cbe4d99d3a8fe
Carol <carol.tsai0224@gmail.com>
2022-05-18 14:01:13 -0400

    added project component

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import '../styles/Portfolio.css';
+import Project from "./Project";
+
+const projectsJson = require("../assets/projects.json");
 
 export default function Portfolio() {
    return (
@@ -7,62 +9,14 @@ export default function Portfolio() {
          <h1>Portfolio</h1>
          <div class="container">
             <div class="row">
-               <div class="card col m-1">
-                  <img src="./images/travelCheck.png" class="card-img-top project-image" alt="..." />
-                  <div class="card-body">
-                     <h5 class="card-title">Travel Check</h5>
-                     <p class="card-text">Javascript</p>
-                     <a href="https://qkeddy.github.io/travelcheck/" target="_blank" rel="noreferrer" class="btn btn-primary m-1">Go to Project</a>
-                     <a href="https://github.com/qkeddy/travelcheck" target="_blank" rel="noreferrer" class="btn btn-secondary m-1">Github</a>
-                  </div>
-               </div>
-               <div class="card col m-1">
-                  <img src="./images/planner.png" class="card-img-top project-image" alt="..." />
-                  <div class="card-body">
-                     <h5 class="card-title">Day Planner</h5>
-                     <p class="card-text">Javascript</p>
-                     <a href="https://github.com/carol-tsai/day-planner" target="_blank" rel="noreferrer" class="btn btn-primary m-1">Go to Project</a>
-                     <a href="https://carol-tsai.github.io/day-planner/" target="_blank" rel="noreferrer" class="btn btn-secondary m-1">Github</a>
-                  </div>
-               </div>
-               <div class="card col m-1">
-                  <img src="./images/quiz.png" class="card-img-top project-image" alt="..." />
-                  <div class="card-body">
-                     <h5 class="card-title">Harry Potter Quiz</h5>
-                     <p class="card-text">Javascript</p>
-                     <a href="https://carol-tsai.github.io/harry-potter-quiz/" target="_blank" rel="noreferrer" class="btn btn-primary m-1">Go to Project</a>
-                     <a href="https://github.com/carol-tsai/harry-potter-quiz" target="_blank" rel="noreferrer" class="btn btn-secondary m-1">Github</a>
-                  </div>
-               </div>
+               <Project key={projectsJson[0].name} project={projectsJson[0]} />
+               <Project key={projectsJson[1].name} project={projectsJson[1]} />
+               <Project key={projectsJson[2].name} project={projectsJson[2]} />
             </div>
             <div class='row'>
-               <div class="card col m-1">
-                  <img src="./images/tunes.png" class="card-img-top project-image" alt="..." />
-                  <div class="card-body">
-                     <h5 class="card-title">Mood Tunes</h5>
-                     <p class="card-text">Javascript</p>
-                     <a href="https://intense-falls-14504.herokuapp.com/" target="_blank" rel="noreferrer" class="btn btn-primary m-1">Go to Project</a>
-                     <a href="https://github.com/carol-tsai/nostalgia-tunes" target="_blank" rel="noreferrer" class="btn btn-secondary m-1">Github</a>
-                  </div>
-               </div>
-               <div class="card col m-1">
-                  <img src="./images/password.png" class="card-img-top project-image" alt="..." />
-                  <div class="card-body">
-                     <h5 class="card-title">Password Generator</h5>
-                     <p class="card-text">Javascript</p>
-                     <a href="https://carol-tsai.github.io/password-generator/" target="_blank" rel="noreferrer" class="btn btn-primary m-1">Go to Project</a>
-                     <a href="https://github.com/carol-tsai/password-generator" target="_blank" rel="noreferrer" class="btn btn-secondary m-1">Github</a>
-                  </div>
-               </div>
-               <div class="card col m-1">
-                  <img src="./images/weather.png" class="card-img-top project-image" alt="..." />
-                  <div class="card-body">
-                     <h5 class="card-title">Weather Dashboard</h5>
-                     <p class="card-text">Javascript</p>
-                     <a href="https://carol-tsai.github.io/weather-dashboard/" target="_blank" rel="noreferrer" class="btn btn-primary m-1">Go to Project</a>
-                     <a href="https://github.com/carol-tsai/weather-dashboard" target="_blank" rel="noreferrer" class="btn btn-secondary m-1">Github</a>
-                  </div>
-               </div>
+               <Project key={projectsJson[3].name} project={projectsJson[3]} />
+               <Project key={projectsJson[4].name} project={projectsJson[4]} />
+               <Project key={projectsJson[5].name} project={projectsJson[5]} />
             </div>
          </div>
          
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,5 +1,36 @@
 import React, { useState } from 'react';
+import '../styles/Project.css';
 
-export default function Project() {
+export default function Project(props) {
+   return (
+      <div className="card col m-1">
+         <img
+            className="card-image"
+            src={props.project.image}
+            alt={props.project.name}
+         />
+         <div className="card-body">
+            <h3 className="card-title">{props.project.name}</h3>
+            <p class="card-text">{props.project.technology}</p>
+            <a
+               href={props.project.deployed_link}
+               rel="noreferrer"
+               target="_blank"
+               className="card-link btn btn-primary m-1"
+            >
+               Visit the project
+            </a>
+            <a
+               href={props.project.github_link}
+               rel="noreferrer"
+               target="_blank"
+               className="card-link btn btn-secondary m-1"
+               class="btn btn-secondary m-1"
+            >
+               GitHub
+            </a>
+         </div>
    
+      </div>
+   );
 }
