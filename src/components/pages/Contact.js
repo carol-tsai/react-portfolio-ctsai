ecab33b6d69f77b28576c04747b83451413e8213
Carol <carol.tsai0224@gmail.com>
2022-05-18 10:41:44 -0400

    create projects.json

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 
 function validateEmail(email) {
-   var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return re.test(String(email).toLowerCase());
+   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return regex.test(String(email).toLowerCase());
 }
 
 export default function Contact() {
