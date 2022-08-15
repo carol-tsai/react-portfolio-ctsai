7ab13345ecc8d4a948dbfdfde2aad32d70878a47
Carol <carol.tsai0224@gmail.com>
2022-08-15 16:35:48 -0400

    removed extra code

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,45 +1,9 @@
-import React, {useState} from 'react';
+
 import '../styles/Contact.css';
 
-function validateEmail(email) {
-   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return regex.test(String(email).toLowerCase());
-}
 
 export default function Contact() {
-   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-   const [errorMessage, setErrorMessage] = useState('');
-   const { name, email, message } = formState;
-
-   const handleSubmit = (e) => {
-      if (!errorMessage) {
-         console.log('Submit Form', formState);
-         setFormState('');
-      }
-      
-   };
-
-   const handleChange = (e) => {
-      if (e.target.name === 'email' && e.target.value.length) {
-         const isValid = validateEmail(e.target.value);
-         if (!isValid) {
-            setErrorMessage('Your email is invalid.');
-         } else {
-            setErrorMessage('');
-         }
-      } else {
-         if (!e.target.value.length) {
-            setErrorMessage(`${e.target.name} is required.`);
-         } else {
-            setErrorMessage('');
-         }
-      }
-      if (!errorMessage) {
-         setFormState({ ...formState, [e.target.name]: e.target.value });
-         console.log('Handle Form', formState);
-      }
-   };
 
    return (
       <div className='m-3'>
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import '../styles/Project.css';
 
 export default function Project(props) {
